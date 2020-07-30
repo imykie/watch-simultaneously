1dcae91e5ec45019dab3c94454373e5a16d4d4ad
Michael Olatunji <tundexmike@gmail.com>
2020-07-30 20:57:18 +0100

    custome youtube controls

--- public/js/controls.js
+++ public/js/controls.js
@@ -1,4 +1,14 @@
 //  This code loads the IFrame Player API code asynchronously.
+
+const progressBar = $(".progressbar");
+const progressBarWidth = document.querySelector(".progressbar").clientWidth;
+const progressBox = $(".progress-box");
+const play = $("#play");
+const pause = $("#pause");
+
+let goPosition;
+let player;
+
 const tag = document.createElement("script");
 
 tag.src = "https://www.youtube.com/iframe_api";
@@ -7,7 +17,6 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // 3. This function creates an <iframe> (and YouTube player)
 //    after the API code downloads.
-let player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player("ytplayer", {
     height: "390",
@@ -30,6 +39,11 @@ function onYouTubeIframeAPIReady() {
 // The API will call this function when the video player is ready.
 function onPlayerReady(event) {
   event.target.playVideo();
+  setInterval(() => {
+    let videoState = (player.getCurrentTime() / player.getDuration()) * 100;
+    let destination = (videoState / 100) * progressBarWidth - 10;
+    progressBox.css("margin-left", destination);
+  }, 200);
 }
 
 // The API calls this function when the player's state changes.
@@ -38,10 +52,35 @@ function onPlayerReady(event) {
 let done = false;
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING && !done) {
-    setTimeout(stopVideo, 6000);
+    // setTimeout(stopVideo, 10000);
     done = true;
   }
 }
 function stopVideo() {
   player.stopVideo();
 }
+
+progressBar.click(function (event) {
+  const divOffset = $(this).offset();
+  let currentPosition = event.clientX - divOffset.left;
+  videoState = Number(((currentPosition / progressBarWidth) * 100).toFixed(2));
+  destination = (videoState / 100) * progressBarWidth - 10;
+  currentState = Math.round((videoState / 100) * player.getDuration());
+  player.seekTo(currentState, true);
+  progressBox.css("margin-left", destination);
+  console.log(
+    divOffset,
+    currentPosition,
+    videoState,
+    destination,
+    currentState
+  );
+});
+
+play.click(function () {
+  player.playVideo();
+});
+
+pause.click(function () {
+  player.pauseVideo();
+});
