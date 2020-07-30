59f2d7564e52e5c167215afdf06b5acf1edeccaf
Michael Olatunji <tundexmike@gmail.com>
2020-07-30 16:21:55 +0100

    adding ejs files

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express();
 // add favicon
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
