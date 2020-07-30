d2ed251b2af09b348839d679a06ef12ff64a6354
Michael Olatunji <tundexmike@gmail.com>
2020-07-30 15:12:29 +0100

    handling error

--- app.js
+++ app.js
@@ -1,8 +1,57 @@
 const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const request = require("request");
+
 const app = express();
 
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("Simultaneouly!");
+    res.send("Simultaneouly!");
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
 });
 
 module.exports = app;
