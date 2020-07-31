c73d0510b84c00288d14d81bff39e4799bd88388
Michael Olatunji <tundexmike@gmail.com>
2020-07-31 17:46:40 +0100

    Adding socker io, volume icons

--- public/js/slider.js
+++ public/js/slider.js
@@ -1,12 +1,29 @@
 const slider = document.getElementById("slider");
+const volumeUp = $("#volume-up");
+const volumeDown = $("#volume-down");
+const volumeOff = $("#volume-off");
 
 slider.oninput = function () {
-  console.log(this.value);
+  let volume = this.value;
+  if (volume > 50) {
+    volumeOff.css("display", "none");
+    volumeDown.css("display", "none");
+    volumeUp.css("display", "block");
+  } else if (volume > 0 && volume <= 50) {
+    volumeUp.css("display", "none");
+    volumeOff.css("display", "none");
+    volumeDown.css("display", "block");
+  } else {
+    volumeUp.css("display", "none");
+    volumeDown.css("display", "none");
+    volumeOff.css("display", "block");
+  }
+  player.setVolume(volume);
   $(".count")
-    .text(this.value)
+    .text(volume)
     .css({
-      left: this.value + "%",
-      transform: "translateX(-" + this.value + "%)"
+      left: volume + "%",
+      transform: "translateX(-" + volume + "%)"
     });
-  $(".fill").css("width", this.value + "%");
+  $(".fill").css("width", volume + "%");
 };
